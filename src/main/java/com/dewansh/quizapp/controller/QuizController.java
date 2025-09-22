1f68e17014b5d27726b3b8b388f627525ef00609
Dewansh <Dewansh@innovasolutions.com>
2025-09-22 16:51:58 +0530

    fix: correct import statement for QuizService and update QuestionDao to use model package

--- src/main/java/com/dewansh/quizapp/controller/QuizController.java
+++ src/main/java/com/dewansh/quizapp/controller/QuizController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dewansh.quizapp.service.quizService;
+import com.dewansh.quizapp.service.QuizService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http;
@@ -19,7 +19,7 @@ public class QuizController {
 
     @Autowired
 
-    quizService QuizService;
+    QuizService QuizService;
 
 
     @PostMapping("/create")
--- src/main/java/com/dewansh/quizapp/dao/QuestionDao.java
+++ src/main/java/com/dewansh/quizapp/dao/QuestionDao.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.dewansh.quizapp.Question;
+import com.dewansh.quizapp.model.Question;
 
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
+
+    List<Question> findRandomQuestionByCategory(String category, int numQ);
 }
